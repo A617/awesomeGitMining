b19aac643e8d2d65a5e8865b05a4fac9ef0c168a
fw14 <fw14@sofware.nju.edu.cn>
2016-03-01 21:03:00 +0800

    Development:HomePageView(part)

--- src/main/ui/MainUI.java
+++ src/main/ui/MainUI.java
@@ -1,5 +1,28 @@
 package main.ui;
 
-public class MainUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainUI extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(MainUI.class,args);
+	}
+
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root = FXMLLoader.load(getClass().getResource("Ui_HomePageView.fxml"));
+		Scene scene = new Scene(root, 800, 600);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setScene(scene);
+        stage.setTitle("JavaFXRange");
+        stage.show();
+
+	}
 
 }
