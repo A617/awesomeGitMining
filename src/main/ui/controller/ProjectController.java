503177f9bcb6da5b1d14591f1a43d9681482042f
fw14 <fw14@sofware.nju.edu.cn>
2016-03-03 09:47:25 +0800

    Development:connect

--- src/main/ui/controller/ProjectController.java
+++ src/main/ui/controller/ProjectController.java
@@ -5,25 +5,33 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import main.ui.MainUI;
 
 public class ProjectController implements Initializable{
-	
+
 	private MainUI app;
-	
+	@FXML
+	private Label projectNameLabel;//用来显示项目的名字
+	@FXML
+	private Label creatorNameLabel;//用来显示创建者的名字
+
 	public void setApp(MainUI app){
 		this.app=app;
 	}
-	
+
 	@FXML
 	private void handle2Menu(){
 		app.gotoHome();
 	}
-
+	public void setProjectLabel(){
+		projectNameLabel.setText("aaa");
+	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
+
 	}
 
 }
