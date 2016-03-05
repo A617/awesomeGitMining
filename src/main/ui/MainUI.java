7a7827755b7cfc45d7510507d9edf66e82fe9339
fw14 <fw14@sofware.nju.edu.cn>
2016-03-05 16:53:39 +0800

    Development:Project view

--- src/main/ui/MainUI.java
+++ src/main/ui/MainUI.java
@@ -14,9 +14,9 @@ public class MainUI extends Application{
 	private Stage stage;
 	private AnchorPane common ;
 	public static AnchorPane homePanel;
-	
+
 	private static MainUI ui;
-	
+
 	@Override
 	/**
 	 * initialize all the fxml document
@@ -24,21 +24,21 @@ public class MainUI extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		this.stage = primaryStage;
 		ui = this;
-		
+
 		common = fxmlLoader.loadPanel("Ui_CommonPart.fxml");
-		homePanel = fxmlLoader.loadPanel("Ui_HomePagePanel.fxml");
+		homePanel = fxmlLoader.loadPanel("Ui_ProjectPanel.fxml");
 		stage.setTitle("awesomeGitmining");
 		stage.setMinWidth(1024);
 		stage.setMinHeight(768);
 		Scene scene = new Scene(common);
 		stage.setScene(scene);
-		scene.getStylesheets().add(MainUI.class.getResource("style/test.css").toExternalForm());
-		//添加图标
-		this.stage.getIcons().add(new Image("file:src/main/ui/style/mark.png"));
+//		scene.getStylesheets().add(MainUI.class.getResource("style/test.css").toExternalForm());
+//		//添加图标
+//		this.stage.getIcons().add(new Image("file:src/main/ui/style/mark.png"));
 		MainController.getInstance().initPanel();
 		stage.show();
 	}
-	
+
 	public static MainUI getUI(){
 		return ui;
 	}
--- src/main/ui/controller/ProjectController.java
+++ src/main/ui/controller/ProjectController.java
@@ -1,30 +1,51 @@
 package main.ui.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
 import main.ui.MainUI;
+import main.vo.RepositoryVO;
 
 public class ProjectController implements Initializable{
-
+	private static ProjectController instance;
 	private MainUI app;
+	@FXML
+	private ScrollPane scrollPane;//所有组件放在这个滚动面板上
 	@FXML
 	private Label projectNameLabel;//用来显示项目的名字
 	@FXML
 	private Label creatorNameLabel;//用来显示创建者的名字
-
+	@FXML
+	private Label profile;//用来显示项目的介绍
+	@FXML
+	private Label star_num;//用来显示star数
+	@FXML
+	private Label fork_num;
+	@FXML
+	private Label colla_num;
+	@FXML
+	private Label sub_num;
+	@FXML
+	private Label contri_num;
 	public void setApp(MainUI app){
 		this.app=app;
+	}
+	public static ProjectController getInstance() {
+		return instance;
 	}
 
 	@FXML
 	private void handleSearch(){
 		setProjectLabel();
 	}
-	
+
 	@FXML
 	public void setProjectLabel(){
 		projectNameLabel.setText("aaa");
@@ -32,8 +53,31 @@ public class ProjectController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-
+		initPane(scrollPane);
 
 	}
+	private void initPane(ScrollPane sp){
+//
+//		FXMLLoader loader = new FXMLLoader();
+//		loader.setLocation(MainUI.class.getResource("config/Ui_SingleReposView.fxml"));
+//		try {
+//			Pane single = (Pane) loader.load();
+//			SingleRepositoryController controller = loader.getController();
+//			RepositoryVO vo = new RepositoryVO();
+//			vo.setName("awesome");
+//			controller.setVO(vo);
+//
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		projectNameLabel.setText("awesome");
+		creatorNameLabel.setText("617");
+		star_num.setText("666");
+		fork_num.setText("666");
+		colla_num.setText("444");
+		sub_num.setText("4");
+		contri_num.setText("88");
 
+	}
 }
