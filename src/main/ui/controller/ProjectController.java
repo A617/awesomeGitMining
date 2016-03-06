7e6e3f27848f50bdefd6f4be954707c3ad2a804e
dtj14 <dtj14@software.nju.edu.cn>
2016-03-06 15:37:30 +0800

    Development:美化界面

--- src/main/ui/controller/ProjectController.java
+++ src/main/ui/controller/ProjectController.java
@@ -6,25 +6,19 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import main.business.service.RepositoryService;
-import main.business.service.UserService;
-import main.ui.MainUI;
+import main.vo.RepositoryVO;
 
-public class ProjectController implements Initializable{
+public class ProjectController implements Initializable {
 	private static ProjectController instance;
-	private RepositoryService repositoryService;
-	private UserService userService;
-
-	private MainUI app;
+	
 
 	@FXML
-	private Label projectNameLabel;//用来显示项目的名字
+	private Label projectNameLabel;// 用来显示项目的名字
 
 	@FXML
-	private Label profile;//用来显示项目的介绍
+	private Label profile;// 用来显示项目的介绍
 	@FXML
-	private Label star_num;//用来显示star数
+	private Label star_num;// 用来显示star数
 	@FXML
 	private Label fork_num;
 	@FXML
@@ -34,39 +28,31 @@ public class ProjectController implements Initializable{
 	@FXML
 	private Label contri_num;
 
+	
 
-@FXML
-private Label label;
-	public void setApp(MainUI app){
-		this.app=app;
-	}
+
 	public static ProjectController getInstance() {
 		if (instance == null) {
 			instance = new ProjectController();
 		}
 		return instance;
 	}
-	private void initProjectInfo(String id){
-		//RepositoryVO
-	}
-	@FXML
-	private void handleSearch(){
-		setProjectLabel();
-	}
 
-	@FXML
-	public void setProjectLabel(){
-		projectNameLabel.setText("aaa");
-	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		projectNameLabel.setText("awesome");
-		star_num.setText("666");
-		fork_num.setText("666");
-		colla_num.setText("444");
-		sub_num.setText("4");
-		contri_num.setText("88");
+		instance = this;
 
 	}
 
+	public void setVO(RepositoryVO vo) {
+		if(vo!=null){
+			profile.setText(vo.getDescription());
+			projectNameLabel.setText(vo.getName()+"/");
+			star_num.setText(vo.getSubscribers_count()+"");
+			fork_num.setText(vo.getForks_count()+"");
+			colla_num.setText(vo.getCollaborators_login().size()+"");
+			sub_num.setText(vo.getSubscribers_count()+"");
+			contri_num.setText(vo.getContributors_login().size()+"");
+		}
+	}
 }
