66296db6854adf0d149ac93db70d6b97f25a94de
fw14 <fw14@sofware.nju.edu.cn>
2016-03-06 19:56:23 +0800

    Development:UserView fixed

--- src/main/ui/controller/SingleUserController.java
+++ src/main/ui/controller/SingleUserController.java
@@ -6,10 +6,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import main.ui.utility.fxmlLoader;
 import main.vo.UserVO;
 
 public class SingleUserController implements Initializable {
-	
+
 	@FXML
 	private Label userImage;
 	@FXML
@@ -17,12 +19,22 @@ public class SingleUserController implements Initializable {
 	@FXML
 	private Label company;
 
+	private UserVO vo;
+
+	@FXML
+	public void handleClick() {
+		MainController.getInstance().setGroup("Ui_UserPanel.fxml");
+		UserController.getInstance().setVO(vo);
+	}
+
+
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public void setVO(UserVO user) {
 		if(user!=null) {
 			//TODO
--- src/main/ui/controller/UserController.java
+++ src/main/ui/controller/UserController.java
@@ -1,16 +1,47 @@
 package main.ui.controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
 import main.ui.MainUI;
+import main.vo.RepositoryVO;
+import main.vo.UserVO;
 
 public class UserController implements Initializable{
-	
+	private static UserController instance;
 	private MainUI app;
+
+	@FXML
+	private Label userNameLabel;
+	@FXML
+	private Label joinTime;
+	@FXML
+	private Label Company;
+	@FXML
+	private Label Eff_num;
+	@FXML
+	private Label qua_num;
+	@FXML
+	private Label Tot_num;
+	@FXML
+	private TreeTableColumn Contri_Pro;
+	@FXML
+	private TreeTableColumn Crea_Pro;
+
+	private List<UserVO> contriList;
+	private List<UserVO> creaList;
+
+	public static UserController getInstance() {
+		if (instance == null) {
+			instance = new UserController();
+		}
+		return instance;
+	}
 
 	public void setApp(MainUI app) {
 		this.app = app;
@@ -19,7 +50,18 @@ public class UserController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		instance=this;
+	}
 
+	public void setVO(UserVO vo){
+		if(vo!=null){
+			userNameLabel.setText("");
+			joinTime.setText("");
+			Company.setText("");
+			Eff_num.setText("");
+			qua_num.setText("");
+			Tot_num.setText("");
+		}
 	}
 
 }
