08a34ca1d4b1ebbeff5fc0d2f02a569353418fce
Wei Fan <Wei Fan>
2016-03-23 13:00:03 +0800

    Development:userstats add listener

--- src/main/ui/controller/UserStaPaneController.java
+++ src/main/ui/controller/UserStaPaneController.java
@@ -23,11 +23,23 @@ public class UserStaPaneController implements Initializable {
 	@FXML
 	private Label company;
 	@FXML
+	private Label blank;
+	@FXML
 	private AnchorPane center;
 
+	private String styleStr = "-fx-background-color: ";
+	private String enterColor = "#69b589;";
+	private String baseColor = "#74c996;";
+	private boolean selectType;
+	private boolean selectRegisterTime;
+	private boolean selectCollaborate;
+	private boolean selectCreate;
+	private boolean selectCompany;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		setChart("Ui_UserTypeChar.fxml");
+		//setChart("Ui_UserTypeChar.fxml");
+		blank.setStyle(styleStr + baseColor);
+		selectType();
 
 	}
 
@@ -43,5 +55,136 @@ public class UserStaPaneController implements Initializable {
 		center.getChildren().clear();
 		if (chart != null)
 			center.getChildren().add(chart);
+	}
+
+	@FXML
+	public void enterType(){
+		type.setStyle(styleStr + enterColor);
+	}
+
+	@FXML
+	public void enterRegisterTime(){
+		registerTime.setStyle(styleStr + enterColor);
+	}
+
+	@FXML
+	public void enterCollaborate(){
+		collaborate.setStyle(styleStr + enterColor);
+	}
+
+	@FXML
+	public void enterCreate(){
+		create.setStyle(styleStr + enterColor);
+	}
+	@FXML
+	public void enterCompany(){
+		company.setStyle(styleStr + enterColor);
+	}
+
+
+
+
+	@FXML
+	public void exitType(){
+		if (!selectType) {
+			type.setStyle(styleStr + baseColor);
+		}
+	}
+
+	@FXML
+	public void exitRegisterTime(){
+		if (!selectRegisterTime) {
+			registerTime.setStyle(styleStr + baseColor);
+		}
+	}
+	@FXML
+	public void exitCollaborate(){
+		if (!selectCollaborate) {
+			collaborate.setStyle(styleStr + baseColor);
+		}
+	}
+	@FXML
+	public void exitCreate(){
+		if (!selectCreate) {
+			create.setStyle(styleStr + baseColor);
+		}
+	}
+	@FXML
+	public void exitCompany(){
+		if (!selectCompany) {
+			company.setStyle(styleStr + baseColor);
+		}
+	}
+	@FXML
+	public void selectType() {
+		selectType = true;
+		selectRegisterTime = false;
+		selectCreate = false;
+		selectCompany = false;
+		selectCollaborate=false;
+		enterType();
+		registerTime.setStyle(styleStr + baseColor);
+		create.setStyle(styleStr + baseColor);
+		company.setStyle(styleStr + baseColor);
+		collaborate.setStyle(styleStr + baseColor);
+		setChart("Ui_UserTypeChar.fxml");
+	}
+	@FXML
+	public void selectRegisterTime() {
+		selectType = false;
+		selectRegisterTime = true;
+		selectCreate = false;
+		selectCompany = false;
+		selectCollaborate=false;
+		enterRegisterTime();
+		type.setStyle(styleStr + baseColor);
+		create.setStyle(styleStr + baseColor);
+		company.setStyle(styleStr + baseColor);
+		collaborate.setStyle(styleStr + baseColor);
+		//setChart("Ui_UserTypeChar.fxml");
+	}
+	@FXML
+	public void selectCreate() {
+		selectType = false;
+		selectRegisterTime = false;
+		selectCreate = true;
+		selectCompany = false;
+		selectCollaborate=false;
+		enterCreate();
+		registerTime.setStyle(styleStr + baseColor);
+		type.setStyle(styleStr + baseColor);
+		company.setStyle(styleStr + baseColor);
+		collaborate.setStyle(styleStr + baseColor);
+		//setChart("Ui_UserTypeChar.fxml");
+	}
+
+	@FXML
+	public void selectCompany() {
+		selectType = false;
+		selectRegisterTime = false;
+		selectCreate = false;
+		selectCompany = true;
+		selectCollaborate=false;
+		enterCompany();
+		registerTime.setStyle(styleStr + baseColor);
+		create.setStyle(styleStr + baseColor);
+		type.setStyle(styleStr + baseColor);
+		collaborate.setStyle(styleStr + baseColor);
+		//setChart("Ui_UserTypeChar.fxml");
+	}
+
+	@FXML
+	public void selectCollaborate() {
+		selectType = false;
+		selectRegisterTime = false;
+		selectCreate = false;
+		selectCompany = false;
+		selectCollaborate=true;
+		enterCollaborate();
+		registerTime.setStyle(styleStr + baseColor);
+		create.setStyle(styleStr + baseColor);
+		company.setStyle(styleStr + baseColor);
+		type.setStyle(styleStr + baseColor);
+		//setChart("Ui_UserTypeChar.fxml");
 	}
 }
