b87e6f372d55b05223b7b91243b35037dbe740c9
fw14 <fw14@software.nju.edu>
2016-05-07 21:01:50 +0800

    Developemnt:login

--- AwesomeGitMining-web/src/main/java/edu/nju/dao/IMemberDaoImpl.java
+++ AwesomeGitMining-web/src/main/java/edu/nju/dao/IMemberDaoImpl.java
@@ -1,27 +1,32 @@
 package edu.nju.dao;
 
-import edu.nju.model.Member;
-import edu.nju.model.Recommender;
-
+import edu.nju.model.*;
+import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
-
+import java.util.Map;
+import java.util.HashMap;
 /**
  * Created by fw on 2016/5/7.
  */
+@Service("memberDao")
 public class IMemberDaoImpl implements IMemberDao{
     @Resource
     private MemberMapper mapper;
 
     @Override
     public Member searchMember(Member member) {
+        Map<String, Object> map = createMap();
+        map.put("username",member.getUsername());
+        Member data = mapper.searchMember(member);
+        return data;
 
-        return null;
     }
 
     @Override
     public int addMember(Member member) {
-        return -1;
+        int result=mapper.addMember(member);
+        return result;
     }
 
     @Override
@@ -38,4 +43,18 @@ public class IMemberDaoImpl implements IMemberDao{
     public List<String> getRecommendByOther(String userName) {
         return null;
     }
+
+
+
+    private Map<String, Object> createMap() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        int pageSize = SystemContext.getSize();
+        int pageOffset = SystemContext.getOffset();
+        map.put("pageSize", pageSize);
+        map.put("pageOffset", pageOffset);
+        return map;
+    }
+
+
+
 }
--- AwesomeGitMining-web/src/main/java/edu/nju/dao/MemberMapper.java
+++ AwesomeGitMining-web/src/main/java/edu/nju/dao/MemberMapper.java
@@ -3,18 +3,18 @@ package edu.nju.dao;
 import edu.nju.model.Member;
 
 public interface MemberMapper {
-    int deleteByPrimaryKey(String userid);
-
-    int insert(Member record);
-
-    int insertSelective(Member record);
-
-    Member selectByPrimaryKey(String userid);
-
-    int updateByPrimaryKeySelective(Member record);
-
-    int updateByPrimaryKey(Member record);
-
-
-
+//    int deleteByPrimaryKey(String userid);
+//
+//    int insert(Member record);
+//
+//    int insertSelective(Member record);
+//
+//    Member selectByPrimaryKey(String userid);
+//
+//    int updateByPrimaryKeySelective(Member record);
+//
+//    int updateByPrimaryKey(Member record);
+//
+    Member searchMember(Member member);
+    int addMember(Member member);
 }
