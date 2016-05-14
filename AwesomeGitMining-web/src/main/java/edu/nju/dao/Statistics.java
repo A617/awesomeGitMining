101c4ea9a81b6ac491f5a77a0f2496dd17f7a87e
dtj14 <dtj14@software.nju.edu.cn>
2016-05-14 14:59:01 +0800

    改一下pom跑jenkins

---
+++ AwesomeGitMining-web/src/main/java/edu/nju/dao/Statistics.java
@@ -0,0 +1,11 @@
+package edu.nju.dao;
+
+import java.util.List;
+
+/**
+ * Created by tj on 2016/5/14.
+ */
+public class Statistics {
+    public static final String[] categories = {"API", "Django", "gem", "jQuery", "Web", "IRC", "Plugin", "database", "Android", "Git",
+            "Emacs", "Linux", "JSON", "toolkit", ".NET", "OS", "XML", "iOS", "MVC", "vim", "Apache", "Maven", "MySQL"};
+}
