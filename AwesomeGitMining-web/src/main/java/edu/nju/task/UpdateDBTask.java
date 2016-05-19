9b69ae8af10454d29732cc212216d714755e10da
dy14 <dy14@software.nju.edu.cn>
2016-05-19 13:20:23 +0800

    Debug:tag language的bug

--- AwesomeGitMining-web/src/main/java/edu/nju/task/UpdateDBTask.java
+++ AwesomeGitMining-web/src/main/java/edu/nju/task/UpdateDBTask.java
@@ -1,8 +1,11 @@
 package edu.nju.task;
 
+import edu.nju.dao.RepoDaoImpl;
 import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Dora on 2016/5/17.
@@ -11,10 +14,13 @@ import org.springframework.stereotype.Component;
 public class UpdateDBTask {
     private static final Logger LOG = Logger.getLogger(UpdateDBTask.class);
 
+    @Resource
+    RepoDaoImpl dao;
+
     //每分钟的10秒执行
     @Scheduled(cron = "10 * * * * ?")
     public void job(){
-
+//        LOG.info(dao.getAllFullname().get(1));
        // LOG.info("hello。。。。");
     }
 
